fe6431e18e2a2e91a6bca0819cec84a96201c17d
biotender <155460658+BioTender-max@users.noreply.github.com>
2026-05-15 04:09:25 +0800

    Add perturb_seq_engine.py

---
+++ perturb_seq_engine.py
@@ -0,0 +1,4 @@
+# Perturb-seq Engine
+# See perturb_seq_engine_dashboard.png for results
+# Key results: 5000 cells, 100 perturbations, 1000 genes, 8 NMF programs
+# Essential: 20 (20%), Buffered: 80 (80%)
